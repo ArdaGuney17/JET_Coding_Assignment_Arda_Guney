16a773abccd4ef9a1024833132069e14d36d0bed
Arda <ardagney05@gmail.com>
2026-04-06 23:49:46 +0200

    feat: implement data fetching with custom headers to prevent 403 errors, and add status code validation

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,24 @@ import requests
 # Initialize the FastAPI application for JET Backend
 JET_app = FastAPI()
 
-# Define the skeleton for the API
+# Define the API URL for Just Eat API
+API_URL = "https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/CT12EH"
+
+# Define the endpoint for the API
 @JET_app.get("/")
+# Define the function to extract restaurants data
 def extract_restaurants_data():
-    return {"message": "JET Backend Operational"}
+    # Define the headers for the API request
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/120.0.0.0",
+        "X-Project-Name": "Arda-JET-Technical-Assessment"
+    }
+    # Send the GET request to the API
+    response = requests.get(API_URL, headers=headers)
+    
+    # Check if the request was successful
+    if response.status_code == 200:
+        return response.json()
+    
+    # Return an error message if the request was not successful
+    return {"message": "Failed to extract data", "status_code": response.status_code}
