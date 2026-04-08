b1e355bf8c4b2bf2e501a9f45fc789bc747fdf2e
Arda <ardagney05@gmail.com>
2026-04-08 13:10:44 +0200

    feat: initialize React frontend with Vite, remove starter code from App.jsx, and add a simple header

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Just Eat Takeaway Coding Assignment</h1>
+      <p>Will fetch restaurants from the Just Eat API</p>
+    </div>
+  )
+}
+
+export default App
